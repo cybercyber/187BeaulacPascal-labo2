cfd7d4ab3cba5cae0c4b48633fe21f338fcdb326
199364921 <nissaba@gmail.com>
2014-06-13 16:32:51 -0400

    MISE A JOUR

--- app/src/main/java/com/example/labo2/app/ContactsDatabaseHandler.java
+++ app/src/main/java/com/example/labo2/app/ContactsDatabaseHandler.java
@@ -8,10 +8,9 @@ import android.content.Context;
 import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.ListView;
 
-/**
- * Created by 199364921 on 2014-06-13.
- */
+import java.util.List;
 
 public class ContactsDatabaseHandler extends SQLiteOpenHelper {
     private static final int VERSION_BD = 1;
@@ -20,22 +19,25 @@ public class ContactsDatabaseHandler extends SQLiteOpenHelper {
     private static final String CHAMP_ID = "id";
     private static final String CHAMP_NOM = "nom";
     private static final String CHAMP_TELEPHONE = "telephone";
-    
-    public ContactsDatabaseHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
-    }
 
-    public ContactsDatabaseHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version, DatabaseErrorHandler errorHandler) {
-        super(context, name, factory, version, errorHandler);
+
+    public ContactsDatabaseHandler(Context context) {
+        super(context, NOM_BD, null, VERSION_BD);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+        sqLiteDatabase.execSQL("CREATE TABLE "+TABLE_CONTACTS+"(id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "name VARCHAR(50) NOT NULL, telephone VARCHAR(20) NOT NULL)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i2) {
 
+    }
+
+    public List<Contact> getAllContacts(){
+
+        return null;
     }
 }
